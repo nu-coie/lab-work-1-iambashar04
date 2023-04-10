981566dfab7de0d7332b9ad5b0ccade6e214db94
iambashar04 <130348671+iambashar04@users.noreply.github.com>
2023-04-10 08:12:31 +0000

    ddd, Apr 4/10/2023, YYYY, 8:12 A +0

--- hello.c
+++ hello.c
@@ -4,9 +4,9 @@ int main(void)
 {
 
   // Step ( 1 ) : Print Hello, World
-  stdio.h
-  #include
-  pritf("hello,world//n")
+#include <stdio.h>
+ int main(void)
+  pritf("hello,world\n")
 
 
   //Step ( 2 ) : Print Hello, Your Name

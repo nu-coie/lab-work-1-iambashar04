decc3bf6d79b1d5b132153760977959f39b95bfc
iambashar04 <130348671+iambashar04@users.noreply.github.com>
2023-04-10 08:17:08 +0000

    ddd, Apr 4/10/2023, YYYY, 8:17 A +0

--- calculator.c
+++ calculator.c
@@ -8,8 +8,7 @@ printf("hello,world!")
 
 {
 // Accept Input From user (char), where (a) stands for "Addition", (m) for "Multiplications," and (d) for "Division,"
-
-
+char input = get_char("op)
 
 
 }

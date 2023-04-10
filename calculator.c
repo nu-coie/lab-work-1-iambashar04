1cae55f9fb1832bd9690442ce565addfea511717
iambashar04 <130348671+iambashar04@users.noreply.github.com>
2023-04-10 08:03:58 +0000

    ddd, Apr 4/10/2023, YYYY, 8:03 A +0

--- calculator.c
+++ calculator.c
@@ -1,12 +1,14 @@
+
 #include <stdio.h>
 #include <cs50.h>
+printf("hello,world!")
 
 
 int main(void)
 {
 // Accept Input From user (char), where (a) stands for "Addition", (m) for "Multiplications," and (d) for "Division,"
-  
 
-  
+
+
 
 }

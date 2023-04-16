7ae0c0637070927089962519407fcb7cb3fc8491
iambashar04 <130348671+iambashar04@users.noreply.github.com>
2023-04-16 23:40:11 +0000

    ddd, Apr 4/16/2023, YYYY, 11:40 A +0

--- hello.c
+++ hello.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int main(void)
-{
+
 
   // Step ( 1 ) : Print Hello, World
 #include <stdio.h>
- int main(void)
-  pritf("hello,world\n")
 
+ {
+  int main(void)
+  pritf("hello,world\n");
+  return 0;
+ }
 
   //Step ( 2 ) : Print Hello, Your Name
 
-}
+
 int main (void)
 printf(")

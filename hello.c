9ca53be4c7328d148cabc39028fc078a0944998a
Saad Badie <107682899+saad-badie@users.noreply.github.com>
2023-04-29 13:13:55 +0000

    ddd, Apr 4/29/2023, YYYY, 1:13 A +0

--- hello.c
+++ hello.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
-
-
   // Step ( 1 ) : Print Hello, World
-#include <stdio.h>
-
  {
-  int main(void)
+
   pritf("hello,world\n");
-  return 0;
+
  }
 
   //Step ( 2 ) : Print Hello, Your Name

eb688634dfbaf1ab0489857e57fb8016cb7f7d93
iambashar04 <130348671+iambashar04@users.noreply.github.com>
2023-05-21 18:39:41 +0000

    ddd, May 5/21/2023, YYYY, 6:39 A +0

--- hello.c
+++ hello.c
@@ -3,6 +3,25 @@
 int main(void){
    int number=get_int("enter the number: ");
    for(int i=1;i<=number;i++)
-     printf("*i");
+     number*=i;
+     {
+      return number;
+     }
+   }
+   int main (void)
+   
+      int main(void) {
+    int number;
 
+    printf("Enter a number: ");
+    number = get_int("");
+
+    if (number < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        int factorial = calculateFactorial(number);
+        printf("%d! = %d\n", number, factorial);
+    }
+
+    return 0;
    }

41922f6b1ec14fb78ce81098d07a1b69f46c9098
iambashar04 <130348671+iambashar04@users.noreply.github.com>
2023-05-21 18:19:04 +0000

    ddd, May 5/21/2023, YYYY, 6:19 A +0

--- hello.c
+++ hello.c
@@ -1,16 +1,5 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-  // Step ( 1 ) : Print Hello, World
- {
-int x = get_int("X:   ");
-int y = get_int("Y:   ");
-
-  printf("%i\n",x+y);
-
- }
-
-  //Step ( 2 ) : Print Hello, Your Name
-
-
+#include<stdio.h>
+#include<cs50.h>
+int main(void){
+   int number=get_int("enter the number: ");
+   for("int i=1")
